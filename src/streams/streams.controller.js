3abc02a34ea413f20b151fc9370467e49c0d8bd5
Alex Perkins <acperkins@gmail.com>
2016-05-14 14:35:08 -0400

    Added default icon for streamers that don't have one set.

--- src/streams/streams.controller.js
+++ src/streams/streams.controller.js
@@ -10,6 +10,7 @@
         console.log('$stateParams', $stateParams);
         var that = this;
         this.singleGameStreamsArray = null;
+        this.noUserIcon = {img: 'http://s.jtvnw.net/jtv_user_pictures/hosted_images/GlitchIcon_WhiteonPurple.png'};
 
         var p = TwitchService.getSingleGameStreams($stateParams.id, 10);
 
