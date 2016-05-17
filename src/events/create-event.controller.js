6e6dac421b05578d71958f2626b185b953c81a74
Alex Perkins <acperkins@gmail.com>
2016-05-17 10:08:12 -0400

    added .then and .catch to create event function.

--- src/events/create-event.controller.js
+++ src/events/create-event.controller.js
@@ -5,43 +5,26 @@
       .module('app')
       .controller('CreateEventController', CreateEventController);
 
-    CreateEventController.$inject = ['EventsService'];
-    function CreateEventController(EventsService) {
-      console.log('in CreateEventController');
-
+    CreateEventController.$inject = ['EventsService', 'NavService'];
+    function CreateEventController(EventsService, NavService) {
 
       this.singleGameEvents = EventsService.singleGameEvents;
+      this.gameList = NavService.navArray;
       var that = this;
       this.newEvent = null;
-
+      this.errorMessage = '';
 
       this.addEvent = function addEvent() {
-        console.log('in createEvent function');
-        console.log(that.newEvent);
-        EventsService.createEvent(that.newEvent);
+        EventsService.createEvent(that.newEvent)
+          .then(function(ref) {
+            console.log('in promise', ref);
+          })
+          .catch(function(err) {
+            console.log('catch error', err);
+            that.errorMessage = 'The server is not responding. Please try again shortly.';
+          });
       };
 
-      // var ref = new Firebase("https://incandescent-heat-8431.firebaseio.com/events");
-      //
-      // ref.orderByChild("game").equalTo('Street Fighter V').on("child_added", function(snapshot) {
-      //   console.log(snapshot.val());
-      // });
-
-      // console.log(EventsService.getEvents);
-      console.log(EventsService.getGameEvents('Street Fighter V'));
-        // .then(console.log('in controller', EventsService.singleGameEvents));
-
-
-      this.login = function login() {
-        var ref = new Firebase("https://incandescent-heat-8431.firebaseio.com");
-        ref.authWithOAuthPopup("google", function(error, authData) {
-          if (error) {
-            console.log("Login Failed!", error);
-          } else {
-            console.log("Authenticated successfully with payload:", authData);
-          }
-        });
-      };
 
     }
 
