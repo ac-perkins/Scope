c748b17a423a2e0fa76fa6c1ffe34e3bb4dbe116
Alex Perkins <acperkins@gmail.com>
2016-05-17 10:00:27 -0400

    Added catches to the edit event and delete event functions.

--- src/events/edit-single-event.controller.js
+++ src/events/edit-single-event.controller.js
@@ -11,21 +11,39 @@
         var that = this;
         this.event = null;
         this.gameList = NavService.navArray;
+        this.errorMessage = '';
 
         EventsService.getEventObject($stateParams.id)
           .then(function(eventObj) {
             console.log(eventObj);
             that.event = eventObj;
-            console.log('that.event in .then', that.event);
+          })
+          .catch(function(err) {
+            console.log('catch error', err);
+            that.errorMessage = 'The server is not responding. Please try again shortly.';
           });
 
         this.editEvent = function editEvent() {
           console.log('that.event', this.event);
-          EventsService.editEventObject($stateParams.id, that.event);
+          EventsService.editEventObject($stateParams.id, that.event)
+            .then(function(ref) {
+              console.log('in editEvent promise', ref);
+            })
+            .catch(function(err) {
+              console.log('catch error', err);
+              that.errorMessage = 'The server is not responding. Please try again shortly.';
+            });
         };
 
         this.deleteEvent = function deleteEvent() {
-          EventsService.deleteEventObject($stateParams.id);
+          EventsService.deleteEventObject($stateParams.id)
+            .then(function(ref) {
+              console.log('in deleteEvent promise', ref);
+            })
+            .catch(function(err) {
+              console.log('catch error', err);
+              that.errorMessage = 'The server is not responding. Please try again shortly.';
+            });
         };
 
       }
