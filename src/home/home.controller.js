d75fe6a0f9f48e2eebe202d3d2f55a2c6a46c8d3
Alex Perkins <acperkins@gmail.com>
2016-05-19 17:08:07 -0400

    Began setting up controller for the home view.

---
+++ src/home/home.controller.js
@@ -0,0 +1,27 @@
+(function() {
+    'use strict';
+
+    angular
+      .module('app')
+      .controller('HomeController', HomeController);
+
+      HomeController.$inject = ['EventsService'];
+      function HomeController(EventsService) {
+
+        var that = this;
+        this.upcomingEvents = null;
+        this.errorMessage = '';
+
+        EventsService.getAllEvents()
+          .then(function(events) {
+            that.upcomingEvents = events;
+          })
+          .catch(function (err) {
+            console.log('catch error', err);
+            that.errorMessage = "The server is not responding. Please try again shortly.";
+          });
+
+
+      }
+
+})();
