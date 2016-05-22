79bdb0ac2025b487f0e4e584a9ebdf1771162370
Alex Perkins <acperkins@gmail.com>
2016-05-22 16:50:43 -0400

    Restructed data in Nav Service so that nav bar could be redone based on game genre, not individual game.

--- src/nav/nav.service.js
+++ src/nav/nav.service.js
@@ -7,16 +7,54 @@
 
       function NavService() {
 
-        var navArray = [
+        var allGamesArray = [
+          {name: 'Dota 2'},
+          {name: 'League of Legends'},
+          {name: 'Heroes of the Storm'},
+          {name: 'Street Fighter V'},
+          {name: 'Super Smash Bros. for Wii U'},
+          {name: 'Counter-Strike: Global Offensive'},
+          {name: 'Overwatch'},
+          {name: 'Call of Duty: Black Ops III'},
+          {name: 'StarCraft II'},
+          {name: 'Hearthstone: Heroes of Warcraft'},
+          {name: 'Duelyst'},
+        ];
+
+        var mobaArray = [
           {id: 'Dota 2', name: 'Dota 2'},
-          {id: 'League of Legends', name: 'League of Legends'},
+          {id: 'Heroes of the Storm', name: 'Heroes of the Storm'},
+          {id: 'League of Legends', name: 'League of Legends'}
+        ];
+
+        var fightingArray = [
+          {id: 'Killer Instinct', name: 'Killer Instinct'},
           {id: 'Street Fighter V', name: 'Street Fighter V'},
+          {id: 'Super Smash Bros. for Wii U', name: 'Super Smash Bros.'}
+        ];
+
+        var fpsArray = [
+          {id: 'Call of Duty: Black Ops III', name: 'Call of Duty: Black Ops III'},
           {id: 'Counter-Strike: Global Offensive', name: 'Counter-Strike: GO'},
-          {id: 'Hearthstone: Heroes of Warcraft', name: 'Hearthstone'}
+          {id: 'Overwatch', name: 'Overwatch'}
+        ];
+
+        var rtsArray = [
+          {id: 'StarCraft II', name: 'StarCraft II'}
+        ];
+
+        var ccgArray = [
+          {id: 'Hearthstone: Heroes of Warcraft', name: 'Hearthstone'},
+          {id: 'Duelyst', name: 'Duelyst'}
         ];
 
         return {
-          navArray: navArray
+          allGamesArray: allGamesArray,
+          mobaArray: mobaArray,
+          fightingArray: fightingArray,
+          fpsArray: fpsArray,
+          rtsArray: rtsArray,
+          ccgArray: ccgArray
         };
       }
 
