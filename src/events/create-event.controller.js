edc31826110702ea881ef6a9cb7efd8b27e5eac5
Alex Perkins <acperkins@gmail.com>
2016-05-24 23:21:14 -0400

    Added a return to the addEvent function and named anonymous functions.

--- src/events/create-event.controller.js
+++ src/events/create-event.controller.js
@@ -14,19 +14,19 @@
       this.newEvent = null;
       this.errorMessage = '';
 
-      $scope.$watch('create.newEvent.game', function(v){
+      $scope.$watch('create.newEvent.game', function makeIconSrc(v){
         v = v.replace(/[^\w]+/g, '');
         that.newEvent.iconSrc = v;
         console.log('$watch', that.newEvent.iconSrc);
       });
 
       this.addEvent = function addEvent() {
-        EventsService.createEvent(that.newEvent)
-          .then(function(ref) {
+        return EventsService.createEvent(that.newEvent)
+          .then(function handlePromise(ref) {
             console.log('in promise', ref);
             $state.go('editAllEvents');
           })
-          .catch(function(err) {
+          .catch(function handleError(err) {
             console.log('catch error', err);
             that.errorMessage = 'The server is not responding. Please try again shortly.';
           });
