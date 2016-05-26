2088bdf85e093385841f2aa684a69a36e3297054
Alex Perkins <acperkins@gmail.com>
2016-05-26 12:09:27 -0400

    Changed default image for embeddedStream. Logout function now sets authData to null. Theater mode now changes CSS properties on body and .stream.

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -9,25 +9,17 @@
       function AppController($state, $stateParams, $sce, $window, LoginService) {
         var that = this;
         this.authData = null;
-        this.embeddedStream = $sce.trustAsResourceUrl("/images/default-iframe.png");
+        this.embeddedStream = $sce.trustAsResourceUrl('/images/scope-logo04.png');
         this.errorMessage = '';
         this.streamHeight = 405;
         this.streamWidth = 720;
         this.largeStream = false;
 
-        // var w = angular.element($window);
-        // w.bind('resize', function () {
-        //   that.streamHeight = window.innerHeight * 0.8;
-        //   that.streamWidth = window.innerWidth * 0.75;
-        //   console.log(that.streamWidth);
-        //   console.log('resize');
-        // });
-
 
         this.changeStream = function changeStream(streamName) {
           console.log('in changeStream function');
           console.log(streamName);
-          that.embeddedStream = $sce.trustAsResourceUrl("http://player.twitch.tv/?channel=" + streamName);
+          that.embeddedStream = $sce.trustAsResourceUrl('http://player.twitch.tv/?channel=' + streamName);
         };
 
         this.login = function login() {
@@ -50,24 +42,24 @@
 
         this.logout = function logout() {
           LoginService.logout();
-
+          that.authData = null;
         };
 
         this.theaterMode = function theaterMode() {
           this.largeStream = true;
           this.streamWidth = window.innerWidth * 0.8;
           this.streamHeight = (this.streamWidth / 16) * 9;
-          $('body').css("background-color", "#333");
+          $('body').css({'background-color': '#333'});
           $('iframe').css({'display': 'block', 'margin': '0 auto'});
-          $('.stream').css({"float": "none"});
+          $('.stream').css({'float': 'none', 'margin-top': '1%'});
         };
 
         this.defaultMode = function defaultMode() {
           this.streamHeight = 405;
           this.streamWidth = 720;
           this.largeStream = false;
-          $('body').css("background-color", "#EFEEF7");
-          $('.stream').css({"float": "right"});
+          $('body').css('background-color', '#EFEEF7');
+          $('.stream').css({'float': 'right', 'margin-top': '3%'});
         };
 
       }
