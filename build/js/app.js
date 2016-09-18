621eb88785d7a3dd7b6ebb353a856c1bfb7de806
Alex Perkins <acperkins@gmail.com>
2016-09-18 14:25:14 -0400

    Update build.

--- build/js/app.js
+++ build/js/app.js
@@ -572,7 +572,7 @@
 
         function getSingleGameStreams(gameName, maxStreams) {
           var def = $q.defer();
-          Twitch.api({method: 'streams', params: {game: gameName, limit: maxStreams} }, function(error, list) {
+          Twitch.api({method: 'streams', params: {game: gameName, limit: maxStreams, client_id: 'nda9qwhtllum6kp584qw6m2a3xf81kc'} }, function(error, list) {
             console.log('In getSingleGameStreams success', list.streams);
             if (error) {
               console.log('Error', error);
